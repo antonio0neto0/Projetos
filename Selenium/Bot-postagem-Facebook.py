7f534d2f5ba6d0f33c2e1cc173395423c9d9a651
Antonio Neto <antonio.enge200@gmail.com>
2025-03-05 14:30:40 -0300

    um bot feito em selenium para curtir e comentar no instagram

--- Selenium/Bot-postagem-Facebook.py
+++ Selenium/Bot-postagem-Facebook.py
@@ -68,22 +68,22 @@ botao_entrar = driver.find_element(By.XPATH, "//button[@name='login']")
 sleep(3)
 botao_entrar.click()
 # sleep(5)
-# # encontrar e clicar no campo de postagem
-# campo_status = driver.find_element(By.XPATH,"//div[@class='m9osqain a5q79mjw gy2v8mqq jm1wdb64 k4urcfbm qv66sw1b']")
-# sleep(2)
-# campo_status.click()
-# sleep(5)
-# # Clicar dentro do campo de status
-# dentro_campo_status = driver.find_element(By.XPATH,"//p[@class='i1ao9s8h hcukyx3x oygrvhab cxmmr5t8 kvgmc6g5']")
-# sleep(1)
-# dentro_campo_status.click()
-# sleep(1)
-# # Digitar algo
-# dentro_campo_status.send_keys('Olá, boa tarde a todos!')
-# sleep(3)
-# # Clicar em publicar
-# botao_publicar = driver.find_element(By.XPATH,"//div[@class='l9j0dhe7 du4w35lb j83agx80 pfnyh3mw taijpn5t bp9cbjyn owycx6da btwxx1t3 kt9q3ron ak7q8e6j isp2s0ed ri5dt5u2 rt8b4zig n8ej3o3l agehan2d sk4xxmp2 rq0escxv d1544ag0 tw6a2znq s1i5eluu tv7at329']")
-# sleep(2)
-# botao_publicar.click()
+# encontrar e clicar no campo de postagem
+campo_status = driver.find_element(By.XPATH,"//div[@class='m9osqain a5q79mjw gy2v8mqq jm1wdb64 k4urcfbm qv66sw1b']")
+sleep(2)
+campo_status.click()
+sleep(5)
+# Clicar dentro do campo de status
+dentro_campo_status = driver.find_element(By.XPATH,"//p[@class='i1ao9s8h hcukyx3x oygrvhab cxmmr5t8 kvgmc6g5']")
+sleep(1)
+dentro_campo_status.click()
+sleep(1)
+# Digitar algo
+dentro_campo_status.send_keys('Olá, boa tarde a todos!')
+sleep(3)
+# Clicar em publicar
+botao_publicar = driver.find_element(By.XPATH,"//div[@class='l9j0dhe7 du4w35lb j83agx80 pfnyh3mw taijpn5t bp9cbjyn owycx6da btwxx1t3 kt9q3ron ak7q8e6j isp2s0ed ri5dt5u2 rt8b4zig n8ej3o3l agehan2d sk4xxmp2 rq0escxv d1544ag0 tw6a2znq s1i5eluu tv7at329']")
+sleep(2)
+botao_publicar.click()
 
 input('')
